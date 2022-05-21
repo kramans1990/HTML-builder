6b3affde1843ccf42cb098a05a1f2688924f1a08
RamanSaksonau <kramans1990@outlook.com>
2022-05-21 10:52:02 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,52 @@
+const path = require("path");
+let fs = require('fs');
+let finalPath = path.join(__dirname , 'files');
+let finalPathCopy = path.join(__dirname , 'files-copy');
 
+
+async function  copyDir () {
+  // let s = await fs.rmdir(finalPathCopy, { recursive: true }, () => {
+    fs.mkdir(finalPathCopy, { recursive: true }, () =>{});
+   // });
+   const filenamesCopy = await fs.promises.readdir(finalPathCopy, {withFileTypes: 'true'});
+   for(let i = 0 ;i<filenamesCopy.length;i++){
+    let fullPath = path.join(finalPathCopy, filenamesCopy[i].name);
+
+     fs.stat(fullPath, (error,stat) => {
+        if(error){console.log(error);}
+        else{
+            if(stat.isFile){
+            fs.unlink(fullPath,(error)=>{
+                if(error){           
+                        console.log(error);          
+                }
+                else{
+                console.log('Удалено: ', filenamesCopy[i].name)}
+            }
+                );
+        }
+        }
+     });
+  
+   
+   
+    }
+    const filenames = await fs.promises.readdir(finalPath, {withFileTypes: 'true'}); 
+    for(let i = 0 ;i<filenames.length;i++){
+          let fullPath = path.join(finalPath, filenames[i].name);
+          let fullPathCopy = path.join(finalPathCopy, filenames[i].name);
+          fs.stat(fullPath, (error, stats) => {
+                   if (error) {
+                     console.log(error);
+                   }
+                   else {          
+                  if(stats.isFile() == true){
+                    fs.copyFile(fullPath, fullPathCopy, (err) => {
+                        if (err) throw err;
+                        console.log('скопирован файл ' + filenames[i].name );
+                      });
+                   }     
+                   }
+                });} 
+}
+copyDir();
